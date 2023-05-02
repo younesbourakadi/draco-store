4b61e28ff501a5cbf1a06636c7f167f5cd6530ab
Laurent HINNEKINT <laurent.hinnekint@gmail.com>
2023-05-02 14:48:06 +0200

    Array creation

---
+++ index.js
@@ -0,0 +1,44 @@
+const articles = [
+    {
+        name: "Arbalète",
+        price: 9.80,
+        quantity: 10,
+    },
+    {
+        name: "Armure" ,
+        price: 7.90,
+        quantity: 7,
+    }, 
+    {
+        name: "Arrow",
+        price: 4.50,
+        quantity: 46,
+    },
+    {
+        name: "Calice" ,
+        price: 3.80,
+        quantity: 26,
+    },
+    {
+        name: "Carte",
+        price: 2.50,
+        quantity: 50,
+    },
+    {
+        name:  "Casque" ,
+        price: 9.90,
+        quantity: 8,
+    },
+    {
+        name: "potion" ,
+        price: 5.60,
+        quantity: 36,
+    },
+    {
+        name: "sword" ,
+        price: 10.80,
+        quantity: 14,
+    }
+]
+
+console.log (articles);
