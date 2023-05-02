3e35a4733353aaaf63d10ae93c82202b0e891809
Laurent HINNEKINT <laurent.hinnekint@gmail.com>
2023-05-02 16:44:09 +0200

    Function addItemToCart is working

--- index.js
+++ index.js
@@ -1,51 +1,71 @@
 const articles = [
     {
-        name: "Arbalète",
+        name: "arbalète",
         price: 9.80,
         quantity: 10,
     },
     {
-        name: "Armure" ,
+        name: "armure",
         price: 7.90,
         quantity: 7,
-    }, 
+    },
     {
-        name: "Arrow",
+        name: "arrow",
         price: 4.50,
         quantity: 46,
     },
     {
-        name: "Calice" ,
+        name: "calice",
         price: 3.80,
         quantity: 26,
     },
     {
-        name: "Carte",
+        name: "carte",
         price: 2.50,
         quantity: 50,
     },
     {
-        name:  "Casque" ,
+        name: "casque",
         price: 9.90,
         quantity: 8,
     },
     {
-        name: "potion" ,
+        name: "potion",
         price: 5.60,
         quantity: 36,
     },
     {
-        name: "sword" ,
+        name: "sword",
         price: 10.80,
         quantity: 0,
     }
 ]
 
+let cart = []
 
-function getAllAvailableStoreItems(arr){
-  return arr.filter(item => item.quantity > 0)
+function getAllAvailableStoreItems(arr) {
+    return arr.filter(item => item.quantity > 0)
+}
+
+function addItemToCart(itemName, soltQuantity, price, arr) {
+    let item = { name: itemName, price: price, soltQuantity: soltQuantity }
+    cart.push(item);
 }
 
 
+function getItemPrice(arr, itemName){
+  let price = 0;
+  arr.forEach(i => {
+    if(i.name == itemName) {
+      price = i.price;
+    }
+  });
+  return price;
+}
 
 
+addItemToCart ('potion', 8, getItemPrice (articles, 'potion'), articles);
+// console.log (cart);
+
+addItemToCart ('arrow', 18, getItemPrice (articles, 'arrow'), articles);
+console.log (cart);
