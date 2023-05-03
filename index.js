36f89a47a649cbbda33b4d813a025417fa231282
Younes Bourakadi <younes.bourakadi@gmail.com>
2023-05-03 12:01:19 +0200

    Refactor

--- index.js
+++ index.js
@@ -47,41 +47,30 @@ function getAllAvailableStoreItems(arr) {
     return arr.filter(item => item.quantity > 0)
 }
 
-function addItemToCart(itemName, soldQuantity, price) {
-  const item = { name: itemName, price, soldQuantity };
+function addItemToCart(name, soldQuantity, price) {
+  const item = { name, price, soldQuantity };
   cart.push(item);
 }
 
-// function addItemToCart(itemName, soldQuantity, price) {
-//   const item = { name: itemName, price, soldQuantity };
-//   cart.push(item);
-// }
-
-
-function getItemPrice(arr, itemName) {
-    let price = 0;
-    arr.forEach(i => {
-        if (i.name == itemName) {
-            price = i.price;
-        }
-    });
-    return price;
+function getItemPrice(items, itemName) {
+  const item = items.find(i => i.name === itemName);
+  return item ? item.price : 0;
 }
 
-function convertGoldToSilverAndGold(price) {
+function convertGoldToSilverAndGold(items, itemName) {
+  let price = getItemPrice(items, itemName);
   let gold = Math.floor(price);
   let silver = Math.round((price - gold) * 10);
   return { gold , silver };
 }
 
-// function removeItemFromCart(arr, itemName) {
-//     cart.splice(itemName, 1);
-//     }
+function removeItemFromCart(itemName) {
+     cart.splice(itemName, 1);
+}
 
-function changeQuantityInCart(num) {
-    //     // if (e.key === enter){
-    cart[0].soltQuantity = num;
-    // }
+function changeQuantityInCart(num, name) {
+  let i = cart.filter(item => item.name === name)
+  i[0].soldQuantity = num;
 }
 
 function getCartTotal(cart){
@@ -92,11 +81,6 @@ function getCartTotalVAT(cart){
   const vat = 1.13;
   return Number((getCartTotal(cart) * vat).toFixed(2));
 }
-
-console.log(getCartTotal([{name: 'casque', price: 4.30, soldQuantity: 5}, {name: 'potion', price: 3.57, soldQuantity: 3}]))
-console.log(getCartTotalVAT([{name: 'casque', price: 4.30, soldQuantity: 5}, {name: 'potion', price: 3.57, soldQuantity: 3}]))
-console.log(convertGoldToSilverAndGold(getCartTotalVAT([{name: 'casque', price: 4.30, soldQuantity: 5}, {name: 'potion', price: 3.57, soldQuantity: 3}])))
-
 
 
 
