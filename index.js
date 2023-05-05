5b79b45a28e379265d73df380aa528ab14ec3611
Laurent HINNEKINT <laurent.hinnekint@gmail.com>
2023-05-05 13:48:27 +0200

    Waiting to test the gold and silver coins total HT and TTC

--- index.js
+++ index.js
@@ -1,69 +1,69 @@
-let articles = [
+// let articles = [
 
-    {
-        name: "arbalète",
-        price: 9.80,
-        quantity: 4,
-    },
-    {
-        name: "armure",
-        price: 7.90,
-        quantity: 7,
-    },
-    {
-        name: "flèche",
-        price: 4.50,
-        quantity: 46,
-    },
-    {
-        name: "calice",
-        price: 3.80,
-        quantity: 26,
-    },
-    {
-        name: "carte",
-        price: 2.50,
-        quantity: 50,
-    },
-    {
-        name: "casque",
-        price: 9.90,
-        quantity: 8,
-    },
-    {
-        name: "potion",
-        price: 5.60,
-        quantity: 36,
-    },
-    {
-        name: "épée",
-        price: 10.80,
-        quantity: 8,
-    },
-    {
-        name: "luth",
-        price: 8.20,
-        quantity: 7,
-    },
-    {
-        name: "masse",
-        price: 13.80,
-        quantity: 5,
-    },
-    {
-        name: "torche",
-        price: 14.80,
-        quantity: 16,
-    },
-    {
-        name: "bouclier",
-        price: 11.80,
-        quantity: 4,
-    }
-  ]
-  
+//     {
+//         name: "arbalète",
+//         price: 9.80,
+//         quantity: 4,
+//     },
+//     {
+//         name: "armure",
+//         price: 7.90,
+//         quantity: 7,
+//     },
+//     {
+//         name: "flèche",
+//         price: 4.50,
+//         quantity: 46,
+//     },
+//     {
+//         name: "calice",
+//         price: 3.80,
+//         quantity: 26,
+//     },
+//     {
+//         name: "carte",
+//         price: 2.50,
+//         quantity: 50,
+//     },
+//     {
+//         name: "casque",
+//         price: 9.90,
+//         quantity: 8,
+//     },
+//     {
+//         name: "potion",
+//         price: 5.60,
+//         quantity: 36,
+//     },
+//     {
+//         name: "épée",
+//         price: 10.80,
+//         quantity: 8,
+//     },
+//     {
+//         name: "luth",
+//         price: 8.20,
+//         quantity: 7,
+//     },
+//     {
+//         name: "masse",
+//         price: 13.80,
+//         quantity: 5,
+//     },
+//     {
+//         name: "torche",
+//         price: 14.80,
+//         quantity: 16,
+//     },
+//     {
+//         name: "bouclier",
+//         price: 11.80,
+//         quantity: 4,
+//     }
+//   ]
 
-  
+
+
 async function getJsonData() {
   const response = await fetch("articles.json");
   const data = await response.json();
@@ -76,100 +76,100 @@ async function getArticlesData() {
   const articlesData = await getJsonData();
   const articles = articlesData.articles;
 
-let cart = []
+  let cart = []
 
 
-function getAllAvailableStoreItems(arr) {
-  return arr.filter(item => item.quantity > 0)
-}
-
-function addItemToCart(name, quantity, price) {
-  const item = cart.find(item => item.name === name);
-  if (item) {
-    item.quantity += quantity;
-  } else {
-    cart.push({ name, price, quantity });
+  function getAllAvailableStoreItems(arr) {
+    return arr.filter(item => item.quantity > 0)
   }
-  if (getCartTotalVAT(cart) > 100 && !cart.some(item => item.name === "coffre")) {
-    cart.push({ name: "coffre", price: 0, quantity: 1 });
+
+  function addItemToCart(name, quantity, price) {
+    const item = cart.find(item => item.name === name);
+    if (item) {
+      item.quantity += quantity;
+    } else {
+      cart.push({ name, price, quantity });
+    }
+    if (getCartTotalVAT(cart) > 100 && !cart.some(item => item.name === "coffre")) {
+      cart.push({ name: "coffre", price: 0, quantity: 1 });
+    }
   }
-}
 
 
-function getItemPrice(items, itemName) {
-  const item = items.find(i => i.name === itemName);
-  return item ? item.price : 0;
-}
-
-// function convertGoldToSilverAndGold(items, itemName) {
-//     let price = getItemPrice(items, itemName);
-//     let gold = Math.floor(price);
-//     let silver = Math.round((price - gold) * 10);
-//     return { gold, silver };
-// }
-
-function convertGoldToSilverAndGold(total) {
-  let gold = Math.floor(total);
-  let silver = Math.round((total - gold) * 10);
-  return { gold, silver };
-}
-
-function removeItemFromCart(itemName) {
-  const index = cart.findIndex(item => item.name === itemName);
-  if (index !== -1) {
-    cart.splice(index, 1);
+  function getItemPrice(items, itemName) {
+    const item = items.find(i => i.name === itemName);
+    return item ? item.price : 0;
   }
-}
 
-function changeQuantityInCart(num, name) {
-  let item = cart.find(item => item.name === name);
-  if (item) {
-    item.quantity = num;
+  // function convertGoldToSilverAndGold(items, itemName) {
+  //     let price = getItemPrice(items, itemName);
+  //     let gold = Math.floor(price);
+  //     let silver = Math.round((price - gold) * 10);
+  //     return { gold, silver };
+  // }
+
+  function convertGoldToSilverAndGold(total) {
+    let gold = Math.floor(total);
+    let silver = Math.round((total - gold) * 10);
+    return { gold, silver };
   }
-}
 
-function addQuantityInCart(name) {
-  const items = cart.filter(item => item.name === name);
-  if (items.length === 0) {
-    return;
+  function removeItemFromCart(itemName) {
+    const index = cart.findIndex(item => item.name === itemName);
+    if (index !== -1) {
+      cart.splice(index, 1);
+    }
   }
-  items[0].quantity++;
-}
 
-function removeQuantityInCart(name) {
-  const item = cart.find(item => item.name === name);
-  if (item && item.quantity > 0) {
-    item.quantity--;
-    return item.quantity;
+  function changeQuantityInCart(num, name) {
+    let item = cart.find(item => item.name === name);
+    if (item) {
+      item.quantity = num;
+    }
   }
-  return 0;
-}
 
-function getCartTotal(cart) {
-  return cart.reduce((acc, curr) => acc + curr.price * curr.quantity, 0);
-}
+  function addQuantityInCart(name) {
+    const items = cart.filter(item => item.name === name);
+    if (items.length === 0) {
+      return;
+    }
+    items[0].quantity++;
+  }
 
-function getCartTotalVAT(cart) {
-  const vat = 1.13;
-  return Number((getCartTotal(cart) * vat).toFixed(2));
-}
+  function removeQuantityInCart(name) {
+    const item = cart.find(item => item.name === name);
+    if (item && item.quantity > 0) {
+      item.quantity--;
+      return item.quantity;
+    }
+    return 0;
+  }
+
+  function getCartTotal(cart) {
+    return cart.reduce((acc, curr) => acc + curr.price * curr.quantity, 0);
+  }
+
+  function getCartTotalVAT(cart) {
+    const vat = 1.13;
+    return Number((getCartTotal(cart) * vat).toFixed(2));
+  }
 
 
-const cartTotalTTC = document.getElementById("cart__total-TTC");
-const cartTotalHT = document.getElementById("cart__total-HT");
-const searchBar = document.getElementById("searchBar");
-const itemList = document.getElementById("item__list");
-let searchTerm = "";
+  const cartTotalTTC = document.getElementById("cart__total-TTC");
+  const cartTotalHT = document.getElementById("cart__total-HT");
+  const searchBar = document.getElementById("searchBar");
+  const itemList = document.getElementById("item__list");
+  let searchTerm = "";
 
-let filteredArticles = [];
+  let filteredArticles = [];
 
-function renderArticles() {
-  itemList.innerHTML = "";
-  filteredArticles.forEach(article => { 
-    if (article.quantity > 0) {
-      const item = document.createElement("li");
-      item.className = "card";
-      item.innerHTML = `
+  function renderArticles() {
+    itemList.innerHTML = "";
+    filteredArticles.forEach(article => {
+      if (article.quantity > 0) {
+        const item = document.createElement("li");
+        item.className = "card";
+        item.innerHTML = `
         <h3 class="card__ttl">${article.name}</h3>
         <img class="card__img" src="./img/${article.name}.png" id="${article.name}" data-img-name="${article.name}">
         <div class="card__price">
@@ -181,34 +181,34 @@ function renderArticles() {
           <img src="img/cart.png" alt="panier" class="card__img--cart" id="add-to-cart">
         </button>
       `;
-      itemList.appendChild(item);
-    }
-  });
-}
-function filterArticles() {
-  let searchWords = searchTerm.toLowerCase().split(" ");
-  filteredArticles = [];
-
-  for (let i = 0; i < searchWords.length; i++) {
-    let currentWord = searchWords[i];
-    let matchedArticles = articles.filter(article => {
-      let articleName = article.name.toLowerCase();
-      return articleName.includes(currentWord);
+        itemList.appendChild(item);
+      }
     });
-    filteredArticles.push(...matchedArticles);
+  }
+  function filterArticles() {
+    let searchWords = searchTerm.toLowerCase().split(" ");
+    filteredArticles = [];
+
+    for (let i = 0; i < searchWords.length; i++) {
+      let currentWord = searchWords[i];
+      let matchedArticles = articles.filter(article => {
+        let articleName = article.name.toLowerCase();
+        return articleName.includes(currentWord);
+      });
+      filteredArticles.push(...matchedArticles);
+    }
+
+    filteredArticles = [...new Set(filteredArticles)];
   }
 
-  filteredArticles = [...new Set(filteredArticles)];
-}
+  searchBar.addEventListener("input", () => {
+    searchTerm = searchBar.value;
+    filterArticles();
+    renderArticles();
+  });
 
-searchBar.addEventListener("input", () => {
-  searchTerm = searchBar.value;
-  filterArticles();
+
   renderArticles();
-});
-
-
-renderArticles();
 
 
 
@@ -223,34 +223,51 @@ renderArticles();
 
 
 
-for (let s = 0; s < articles.length ; s++) {
-  if (articles[s].quantity <= 3 && articles[s].quantity >= 1) {
-    // console.log (articles[s].quantity)
-    window.alert(`!!!!!! ${articles[s].name} a moin de 3 pièces en stock. !!!!!! `)
-  } else if (articles[s].quantity === undefined || articles[s].quantity === 0)
-    window.alert(`!!!!!! STOCK EPUISE !!!!!! ${articles[s].name} !!!!!! STOCK EPUISE !!!!!! `)
-}
+  for (let s = 0; s < articles.length; s++) {
+    if (articles[s].quantity <= 3 && articles[s].quantity >= 1) {
+      // console.log (articles[s].quantity)
+      window.alert(`!!!!!! ${articles[s].name} a moin de 3 pièces en stock. !!!!!! `)
+    } else if (articles[s].quantity === undefined || articles[s].quantity === 0)
+      window.alert(`!!!!!! STOCK EPUISE !!!!!! ${articles[s].name} !!!!!! STOCK EPUISE !!!!!! `)
+  }
 
-const cartSilver = document.getElementById("cart__silver");
-const cartGold = document.getElementById("cart__gold");
+  //TTC
+  const cartSilver = document.getElementById("cart__silver");
+  const cartGold = document.getElementById("cart__gold");
 
-const cartList = document.getElementById("cart__list");
-function renderCart() {
-  cartTotalTTC.innerText = getCartTotalVAT(cart).toLocaleString("fr-FR", {
-    maximumFractionDigits: 2
-  });
+  //HT
+  const cartSilverHT = document.getElementById("cart__silver-HT");
+  const cartGoldHT = document.getElementById("cart__gold-HT");
 
-  cartTotalHT.innerText = getCartTotal(cart).toLocaleString("fr-FR", {
-    maximumFractionDigits: 2
-  });
-  const { gold, silver } = convertGoldToSilverAndGold(getCartTotalVAT(cart));
-  cartGold.innerText = gold;
-  cartSilver.innerText = silver;
-  cartList.innerHTML = '';
-  cart.forEach(item => {
-    const cartItem = document.createElement("article");
-    cartItem.className = "itm";
-    cartItem.innerHTML = `
+  const cartList = document.getElementById("cart__list");
+  function renderCart() {
+    cartTotalTTC.innerText = getCartTotalVAT(cart).toLocaleString("fr-FR", {
+      maximumFractionDigits: 2
+    });
+
+    cartTotalHT.innerText = getCartTotal(cart).toLocaleString("fr-FR", {
+      maximumFractionDigits: 2
+    });
+
+
+    const { gold, silver } = convertGoldToSilverAndGold(getCartTotalVAT(cart));
+    const { gold: goldHT, silver: silverHT } = convertGoldToSilverAndGold(getCartTotal(cart));
+
+
+    console.log(cartGoldHT, cartSilverHT);
+    console.log(goldHT, silverHT);
+    console.log(convertGoldToSilverAndGold(getCartTotal(cart)));
+
+
+    cartGold.innerText = gold;
+    cartSilver.innerText = silver;
+    cartGoldHT.innerText = goldHT;
+    cartSilverHT.innerText = silverHT;
+    cartList.innerHTML = '';
+    cart.forEach(item => {
+      const cartItem = document.createElement("article");
+      cartItem.className = "itm";
+      cartItem.innerHTML = `
     
       <div class="itm__wrap">
         <img src="./img/${item.name}.png" alt="${item.name}" class="itm__img">
@@ -266,52 +283,52 @@ function renderCart() {
       </button>
     `;
 
-    const removeButton = cartItem.querySelector("#remove");
-    removeButton.addEventListener("click", () => {
-      removeItemFromCart(item.name);
-      cartItem.remove();
+      const removeButton = cartItem.querySelector("#remove");
+      removeButton.addEventListener("click", () => {
+        removeItemFromCart(item.name);
+        cartItem.remove();
 
-      renderCart()
+        renderCart()
 
+      });
+
+      const inputField = cartItem.querySelector("#input");
+      inputField.addEventListener("change", () => {
+        changeQuantityInCart(inputField.value, item.name);
+
+        renderCart()
+      });
+
+      const addButton = cartItem.querySelector("#inc");
+      addButton.addEventListener("click", () => {
+        addQuantityInCart(item.name);
+        inputField.value = parseInt(inputField.value) + 1;
+        renderCart();
+      });
+
+      const decButton = cartItem.querySelector("#dec");
+      decButton.addEventListener("click", () => {
+        removeQuantityInCart(item.name);
+        inputField.value = parseInt(inputField.value) > 0 ? parseInt(inputField.value) - 1 : 0;
+
+        renderCart()
+
+      });
+      cartList.appendChild(cartItem);
     });
 
-    const inputField = cartItem.querySelector("#input");
-    inputField.addEventListener("change", () => {
-      changeQuantityInCart(inputField.value, item.name);
+  }
 
-      renderCart()
-    });
+  const buttons = document.querySelectorAll("#add-to-cart");
+  buttons.forEach(button => {
+    button.addEventListener("click", event => {
 
-    const addButton = cartItem.querySelector("#inc");
-    addButton.addEventListener("click", () => {
-      addQuantityInCart(item.name);
-      inputField.value = parseInt(inputField.value) + 1;
+      const imgName = event.target.parentElement.parentElement.querySelector(".card__img").dataset.imgName;
+      addItemToCart(imgName, 1, getItemPrice(articles, imgName));
       renderCart();
-    });
-
-    const decButton = cartItem.querySelector("#dec");
-    decButton.addEventListener("click", () => {
-      removeQuantityInCart(item.name);
-      inputField.value = parseInt(inputField.value) > 0 ? parseInt(inputField.value) - 1 : 0;
-
-      renderCart()
 
     });
-    cartList.appendChild(cartItem);
   });
-
-}
-
-const buttons = document.querySelectorAll("#add-to-cart");
-buttons.forEach(button => {
-  button.addEventListener("click", event => {
-
-    const imgName = event.target.parentElement.parentElement.querySelector(".card__img").dataset.imgName;
-    addItemToCart(imgName, 1, getItemPrice(articles, imgName));
-    renderCart();
-
-  });
-});
 
 
 
